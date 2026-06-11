feeb845773510d2c0d2294f5c7e0668aeef84d25
unknown <imdadkhanr9@gmail.com>
2026-06-11 10:12:41 +0500

    image size increase

--- src/server.js
+++ src/server.js
@@ -25,8 +25,8 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser()); // Parse cookies for HTTP-only refresh tokens
 
 // Initialize Socket.io
